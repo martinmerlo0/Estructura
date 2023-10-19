33ac11ed40b7fc51f0cccf3ec24eadce0088a382
martinmerlo0 <dmmb2016v72@gmail.com>
2023-10-19 09:28:37 -0300

    Implementacion de arboles en java

---
+++ Arbolitos/BinaryTreeBasis.java
@@ -0,0 +1,42 @@
+
+
+public interface BinaryTreeBasis{
+
+
+    public Object getRoot();
+
+
+    public void setRoot(Object item);
+
+
+    public boolean isEmpty();
+
+
+
+    public void makeEmpty();
+
+
+
+    public void printPreOrder();
+
+
+    public void printPostOrder();
+
+
+    public void printInOrder();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Arbolitos/TreeNode.java
@@ -0,0 +1,7 @@
+public class TreeNode {
+
+    private Object element;
+    private TreeNode left;
+    private TreeNode right;
+    
+}
